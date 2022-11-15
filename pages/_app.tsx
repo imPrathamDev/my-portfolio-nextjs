f2dd0894a59cec2af3fe4858240fd6586c37d442
Pratham <pratham.sharma2105@gmail.com>
2022-11-15 22:09:43 +0530

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import "../styles/vanilla.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -32,6 +33,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
