fe5b6516e57417ebb9555a6ca2d450e6755c6f68
Pratham <pratham.sharma2105@gmail.com>
2023-02-25 20:07:01 +0530

    fix some issues

--- context/SmoothScroll.context.js
+++ context/SmoothScroll.context.js
@@ -1,39 +1,40 @@
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/dist/ScrollTrigger'
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useState } from "react";
 
-export const SmoothScrollContext = createContext({
-  scroll: null,
-})
+export const SmoothScrollContext = createContext();
 
 export const SmoothScrollProvider = ({ children, options }) => {
-  const [scroll, setScroll] = useState(null)
+  const [scroll, setScroll] = useState(null);
   useEffect(() => {
     if (!scroll) {
-      ; (async () => {
+      (async () => {
         try {
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const LocomotiveScroll = (await import("locomotive-scroll")).default;
 
           setScroll(
             new LocomotiveScroll({
-              el: document.querySelector('[data-scroll-container]') ?? undefined,
+              el:
+                document.querySelector("[data-scroll-container]") ?? undefined,
               ...options,
-              class: 'is-reveal',
+              class: "is-reveal",
             })
-          )
+          );
         } catch (error) {
-          throw Error(`[SmoothScrollProvider]: ${error}`)
+          throw Error(`[SmoothScrollProvider]: ${error}`);
         }
-      })()
+      })();
     }
-
+    console.log(scroll);
     return () => {
-      scroll && scroll.destroy()
-    }
-  }, [scroll]) // eslint-disable-line react-hooks/exhaustive-deps
+      scroll && scroll.destroy();
+    };
+  }, [scroll]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <SmoothScrollContext.Provider value={{ scroll }}>{children}</SmoothScrollContext.Provider>
-}
+  return (
+    <SmoothScrollContext.Provider value={{ scroll }}>
+      {children}
+    </SmoothScrollContext.Provider>
+  );
+};
 
-SmoothScrollContext.displayName = 'SmoothScrollContext'
-SmoothScrollProvider.displayName = 'SmoothScrollProvider'
+SmoothScrollContext.displayName = "SmoothScrollContext";
+SmoothScrollProvider.displayName = "SmoothScrollProvider";
