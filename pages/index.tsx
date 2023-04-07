f9501a0ff509be9ea2ac34c2a67a51b69d95fdc9
Pratham <pratham.sharma2105@gmail.com>
2023-04-08 00:06:44 +0530

    fix main index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,12 @@ import QuoteSection from "../components/sections/QuoteSection";
 import { PostTypes, ProjectTypes } from "../types/types";
 import Head from "next/head";
 import client from "../sanityClient";
+import { useEffect, useState } from "react";
+import Ukiyo from "ukiyojs";
+import ScrollImageSection from "../components/sections/ScrollImageSection";
+import PageTitle from "../components/PageTitle";
+import Layout from "../components/layout/Layout";
+import PreLoader from "../components/preloader";
 
 interface HomeProps {
   posts: PostTypes[];
@@ -33,64 +39,26 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Home: NextPage<HomeProps> = ({ posts, projects }) => {
+  useEffect(() => {
+    const images = document.querySelectorAll(".ukiyo");
+    new Ukiyo(images);
+    return () => {};
+  }, []);
   return (
-    <>
-      <Head>
-        <title>Pratham Sharma</title>
-        <meta name="title" content="Pratham Sharma" />
-        <meta
-          name="description"
-          content="Pratham Sharma is a Web and App Developer, let's explore his projects and blogs. He always looking for fascinating project ideas. He specializes
- in mainly in website development (MERN & NextJS) and application development (Flutter & React Native) so let's see his portfolio website."
-        />
-        <meta
-          name="keywords"
-          content="Pratham Sharma, Portfolio, Pratham, imPrathamDev"
-        />
-        <meta name="author" content="Pratham Sharma" />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={process.env.NEXT_PUBLIC_HOST} />
-        <meta property="twitter:title" content="Pratham Sharma" />
-        <meta
-          property="twitter:description"
-          content="Pratham Sharma is a Web and App Developer, let's explore his projects and blogs. He always looking for fascinating project ideas. He specializes
- in mainly in website development (MERN & NextJS) and application development (Flutter & React Native) so let's see his portfolio website."
-        />
-        <meta
-          property="twitter:image"
-          content={`${process.env.NEXT_PUBLIC_HOST}/ogImages/Design-&-Development-Portfolio.png`}
-        />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_HOST} />
-        <meta property="og:title" content="Pratham Sharma" />
-        <meta
-          property="og:description"
-          content="Pratham Sharma is a Web and App Developer, let's explore his projects and blogs. He always looking for fascinating project ideas. He specializes
- in mainly in website development (MERN & NextJS) and application development (Flutter & React Native) so let's see his portfolio website."
-        />
-        <meta
-          property="og:image"
-          content={`${process.env.NEXT_PUBLIC_HOST}/ogImages/Design-&-Development-Portfolio.png`}
-        />
-      </Head>
-      <div data-scroll-container id="container">
-        <NavBar />
-        <div data-scroll-section>
-          <main className="">
-            <HeroSection />
-            <About />
-            <Credentials />
-            <ProjectsSection projects={projects} />
-            <QuoteSection />
-            <Articles posts={posts} />
-            <Contact />
-          </main>
-          <Footer />
-        </div>
-      </div>
-    </>
+    <Layout>
+      <PageTitle />
+      {/* <PreLoader /> */}
+      <main className="">
+        <HeroSection />
+        <About />
+        <Credentials />
+        <ProjectsSection projects={projects} />
+        <ScrollImageSection />
+        <QuoteSection />
+        <Articles posts={posts} />
+        <Contact />
+      </main>
+    </Layout>
   );
 };
 
