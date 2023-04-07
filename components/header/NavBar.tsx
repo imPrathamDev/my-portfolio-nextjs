cf15fe4d2219af1aa5fc2467f15d1a7db8977935
Pratham <pratham.sharma2105@gmail.com>
2023-04-08 00:13:29 +0530

    nav bar fix

--- components/header/NavBar.tsx
+++ components/header/NavBar.tsx
@@ -1,12 +1,24 @@
 import gsap, { Expo, Power2 } from "gsap";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import HumburgerMenu from "./HumburgerMenu";
+
+const data = [
+  {
+    id: 0,
+    title: "Available for freelancing",
+    link: "",
+  },
+  {
+    id: 1,
+    title: "Checkout My Blogs",
+    link: "/bog",
+  },
+];
 
 const NavBar: React.FC = () => {
   const [show, setShow] = useState(false);
   let tl = gsap.timeline();
-
   const isMobile = (): Boolean => {
     if (
       navigator.userAgent.match(/Android/i) ||
@@ -21,7 +33,6 @@ const NavBar: React.FC = () => {
     }
     return false;
   };
-
   useEffect(() => {
     if (show) {
       tl.to("#nav-container", {
@@ -71,18 +82,21 @@ const NavBar: React.FC = () => {
     <>
       <header
         id="main-header"
-        className="sticky top-0 mx-4 xl:-mb-24 lg:-mb-24 xl:mx-16 py-6 flex items-center justify-between z-[1000] mix-blend-difference"
+        className="sticky top-0 mx-4 xl:-mb-24 lg:-mb-24 xl:mx-16 py-6 flex items-center justify-between mix-blend-difference z-[1000]"
       >
         <Link href={"/"}>
           <a>
-            <h1 className="text-3xl tracking-wide font-dream-avenue mix-blend-difference">
+            <h1
+              data-value="Tatakae"
+              className="text-3xl tracking-wide font-dream-avenue mix-blend-difference will-change-transform heading__transform"
+            >
               Pratham<span className="text-primary">.</span>
             </h1>
           </a>
         </Link>
         <button
           onClick={() => showNav()}
-          className=" flex items-center font-dream-avenue tracking-widest relative text-2xl transform transition-all duration-700 hover:text-primary after:absolute after:h-0.5 after:w-0 after:bg-primary after:transition-all after:duration-700 after:-bottom-0.5 after:left-0 after:rounded-full hover:after:w-full group"
+          className="linkHover flex items-center font-dream-avenue tracking-widest relative text-2xl transform transition-all duration-700 hover:text-primary after:absolute after:h-0.5 after:w-0 after:bg-primary after:transition-all after:duration-700 after:-bottom-0.5 after:left-0 after:rounded-full hover:after:w-full group"
         >
           Menu
           <svg
@@ -100,8 +114,30 @@ const NavBar: React.FC = () => {
             />
           </svg>
         </button>
-        {/*  <NavMenu />
-        <MobileMenu /> */}
+        {/* <div className="flex items-center gap-x-3">
+          <div
+            // id="hero-marquee"
+            className="py-1 rounded-full border border-primary-dark-white max-w-[25vw] bg-primary-black/10 backdrop-blur-lg z-[999999999] linkHover"
+          >
+            <div className="relative flex overflow-x-hidden text-lg">
+              <div className="animate-marquee-hero whitespace-nowrap">
+                <span className="mx-4">Marquee Item 1</span>
+                <span className="mx-4">Marquee Item 2</span>
+                <span className="mx-4">Marquee Item 3</span>
+                <span className="mx-4">Marquee Item 4</span>
+                <span className="mx-4">Marquee Item 5</span>
+              </div>
+
+              <div className="absolute top-0 animate-marquee-hero-2 whitespace-nowrap">
+                <span className="mx-4">Marquee Item 1</span>
+                <span className="mx-4">Marquee Item 2</span>
+                <span className="mx-4">Marquee Item 3</span>
+                <span className="mx-4">Marquee Item 4</span>
+                <span className="mx-4">Marquee Item 5</span>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </header>
       <HumburgerMenu show={show} />
     </>
