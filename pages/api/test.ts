4c28c571afaff9e492745f330b5e460a604bd2d3
Pratham <pratham.sharma2105@gmail.com>
2023-04-10 14:35:23 +0530

    testing api

---
+++ pages/api/test.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+  return res.send(`${req.protocol}://${req.get("host")}`);
+}
